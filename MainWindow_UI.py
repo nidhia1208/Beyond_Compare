d512c05df694f9b3bced54c9c10697748541d8da
nidhia1208 <nidhia1208@gmail.com>
2017-12-20 10:45:49 +0530

    Create MainWindow_UI.py

---
+++ MainWindow_UI.py
@@ -0,0 +1,20 @@
+From Tkinter import *
+From Tkinter import ttk
+
+class MainWindow_UI:
+      fileTreeRow=filePathLabelsRow=0
+      searchTextRow=1
+      uniScrollBarRow=lineNumbersRow=textAreaRow=2
+      horizontalScrollBarRow=3
+      
+      fileTreeCol=0
+      fileTreeScrollBarCol=1
+      leftLineNumbersCol=leftFilePathLabelsCol=2
+      leftTextAreaCol=leftHorizontalScrollBarCol=3
+      uniScrollBarCol=4
+      rightLineNumberCol=rightFilePathLablesCol=5
+      rightTextAreaCol=rightHorizontalScrollBarCol=6
+      
+#colors
+      
+          
